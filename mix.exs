27e6b504a93352621a3917fb4a4c21826fa1cdfc
Dennis Beatty <dennis@dcbeatty.com>
2018-01-25 22:10:16 -0700

    format code

--- mix.exs
+++ mix.exs
@@ -6,9 +6,9 @@ defmodule BullServer.Mixfile do
       app: :bull_server,
       version: "0.0.2",
       elixir: "~> 1.6",
-      elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers,
-      start_permanent: Mix.env == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
@@ -25,7 +25,7 @@ defmodule BullServer.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   #
